cafba6cb06cf4056a61b3e254a6c927a0fe6c36c
AleFarinaIV <Alessandrofarina401@gmail.com>
2024-10-16 21:03:42 +0200

    [UPD] created and setup show to manage a single project

--- app/Http/Controllers/Api/ProjectController.php
+++ app/Http/Controllers/Api/ProjectController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers\Api;
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 use App\Models\Project;
+use Symfony\Component\HttpFoundation\ResponseHeaderBag;
 
 class ProjectController extends Controller
 {
@@ -13,6 +14,24 @@ class ProjectController extends Controller
         return response()->json([
             'success' => true,
             'results' => $projects
+        ]);
+    }
+
+    public function show($slug) {
+        
+        // recupero un progetto con un determinato slug
+        $project = Project::with('type', 'technologies')->where('slug', $slug)->get();
+
+        // controllo che il progetto esista e non sia null
+        if($project) {
+            return response()->json([
+                'success' => true,
+                'result' => $project
+            ]);
+        }
+
+        return response()->json([
+            'success' => false,
         ]);
     }
 }
