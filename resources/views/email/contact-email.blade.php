b23b5dc74ab832da6835fc0e276c1d0f2e67e386
AleFarinaIV <Alessandrofarina401@gmail.com>
2024-10-17 16:26:23 +0200

    [UPD] created contact-email with email structure

---
+++ resources/views/email/contact-email.blade.php
@@ -0,0 +1,9 @@
+<h1>Ciao Alessandro</h1>
+<p>
+    Hai ricevuto una nuova mail da:
+    <p><strong>Name:</strong>{{ $lead->name }}</p>
+    <p><strong>Surname:</strong>{{ $lead->surname }}</p>
+    <p><strong>Telephone:</strong>{{ $lead->phone }}</p>
+    <p><strong>Email:</strong>{{ $lead->email }}</p>
+    <p><strong>Content:</strong>{{ $lead->content }}</p>
+</p>
